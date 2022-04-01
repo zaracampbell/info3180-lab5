8d54785735ce109b4ea8cea2107e6d8d92b0e612
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-04-01 00:25:51 -0400

    Add Bootstrap CSS and AppHeader and AppFooter components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+<script>
+export default {
+    data() {
+        return {
+            message: "Hello World! This is Lab 7"
+        };
+    },
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="container">
+      <div class="text-center">
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+        <h1>{{ message }}</h1>
+      </div>
+    </div>
 </template>
+
+<style>
+/* Add any component specific styles here */
+</style>

2720a0e497fe8cda43f3878574369b4c9a39302f
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-04-02 16:10:47 -0400

    return Flask app instantiation to the default

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from .config import Config
 
-app = Flask(__name__, static_folder='../dist/assets')
+app = Flask(__name__)
 app.config.from_object(Config)
 
 from app import views

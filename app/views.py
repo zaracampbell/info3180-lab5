36f1af6b20b21f1421e4159f51349e9a5b137aa3
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2023-04-01 15:03:27 -0500

    Remove unnecessary app.run call

--- app/views.py
+++ app/views.py
@@ -61,7 +61,3 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
-
-
-if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port="8080")

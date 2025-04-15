3fa218f125f7c223489338be6985723164058d6f
zaracampbell <campbellzara13@gmail.com>
2025-04-15 03:24:55 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 from .config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import views
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+csrf = CSRFProtect(app)
+
+from app import views, models

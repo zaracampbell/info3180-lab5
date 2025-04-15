f92708b666c9643245102c50d2c6592c6668d2ed
zaracampbell <campbellzara13@gmail.com>
2025-04-15 03:20:43 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
+# models.py
+
+from . import db
+from datetime import datetime
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    poster = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 # Add any model classes for Flask-SQLAlchemy here
